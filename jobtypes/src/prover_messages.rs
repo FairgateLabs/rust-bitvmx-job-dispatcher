811735dc7d52785a391a97b9406d3629e1337369
Tomas Szejnfeld Sirkis <tszejnfeld@fi.uba.ar>
2025-07-01 11:35:05 -0300

    You can receive any elf file to use as a ZKP

--- jobtypes/src/prover_messages.rs
+++ jobtypes/src/prover_messages.rs
@@ -6,17 +6,20 @@ use serde::{Deserialize, Serialize};
 impl DispatcherMessage for ProverJobType {
     fn command(&self) -> Result<(String, Vec<String>, String), DispatcherError> {
         match self {
-            ProverJobType::Prove(input_value, _elf, output_file_path) => {
+            ProverJobType::Prove(input_value, elf, output_file_path) => {
                 std::fs::create_dir_all(output_file_path)?;
                 let json = format!("{output_file_path}/output.json");
                 let stark_proof = format!("{output_file_path}/stark_proof.bin");
-                let input_value = u32::from_be_bytes(input_value.as_slice().try_into().unwrap());
+                let input_file = format!("{output_file_path}/input.bin");
+                std::fs::write(&input_file, input_value)?;
+
                 let cmd = "sh".to_string();
                 let args = vec![
                     "-c".to_string(),
                     format!(
                         "../rust-bitvmx-zk-proof/target/release/host prove-stark \
-                        --input {input_value} \
+                        --input {input_file} \
+                        --elf {elf} \
                         --output {stark_proof} \
                         --json {json} \
                         && ../rust-bitvmx-zk-proof/target/release/host \
