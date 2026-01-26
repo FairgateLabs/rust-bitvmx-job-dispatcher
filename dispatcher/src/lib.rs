6547277c06833c9b3876e2e202b672520c29eb0a
Kevin Wahle <62912263+KevinWahle@users.noreply.github.com>
2026-01-26 16:24:48 -0300

    Improve message handling in dispatcher

--- dispatcher/src/lib.rs
+++ dispatcher/src/lib.rs
@@ -28,7 +28,7 @@ use tracing::{debug, info, warn};
 
 use crate::{
     dispatcher_error::DispatcherError,
-    helper::{Msg, PingMessage, persist_job, process_msg},
+    helper::{persist_job, process_msg, Msg, PingMessage},
 };
 
 pub struct DispatcherHandler<T: DispatcherMessage + DeserializeOwned> {
@@ -80,16 +80,13 @@ where
                     PingMessage::Pong => {
                         warn!("Job Dispatcher should not receive Pong");
                         return Ok(false);
-                    },
+                    }
                 }
 
                 let pong = serde_json::to_string(&PingMessage::Pong)?;
 
-                self.channel.send(
-                    &msg.id,
-                    pong,
-                )?;
-            }else {
+                self.channel.send(&msg.id, pong)?;
+            } else {
                 let (child, context) = process_msg(&mut self.dispatcher, &msg.raw)?;
                 persist_job(&context, &msg, self.storage.clone())?;
                 self.workers.push((child, msg.id, context));
@@ -121,15 +118,19 @@ where
                                 self.dispatcher
                                     .process_result(&context.job_id, buf, status)?;
 
-                            self.channel.send(
+                            let result = self.channel.send(
                                 &id,
                                 ResultMessage::new(context.job_id.clone(), result).to_string()?,
                             )?;
 
-                            self.storage
-                                .lock()
-                                .map_err(|_| DispatcherError::MutexPoisoned)?
-                                .remove_job(&context.job_id)?;
+                            if result {
+                                self.storage
+                                    .lock()
+                                    .map_err(|_| DispatcherError::MutexPoisoned)?
+                                    .remove_job(&context.job_id)?;
+                            } else {
+                                warn!("Failed to send job result to client");
+                            }
                             Ok(false)
                         }
                         Ok(None) => Ok(true),
