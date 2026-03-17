33bde96e86af09e73e15d8dda399de77ea4fb4d6
Martin Jonas <martinjonas@gmail.com>
2026-03-16 21:48:48 -0300

    add aws mode to risczer emulator

--- risczero-dispatcher/src/main.rs
+++ risczero-dispatcher/src/main.rs
@@ -4,6 +4,11 @@ use tracing::info;
 
 fn main() -> Result<(), anyhow::Error> {
     let (channel, check_interval, running, storage, config) = init()?;
+
+    #[cfg(feature = "aws")]
+    info!("Running in AWS mode");
+    #[cfg(not(feature = "aws"))]
+    info!("Running in local mode");
     dispatcher_loop::<ProverJobType>(channel, check_interval, running, storage, config)?;
 
     info!("Shutting down...");
